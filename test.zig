0e38d2a9eadc21e4f092667d85f16bf0cec87e98
ItsMeSamey <sameychain5041@gmail.com>
2026-01-20 01:58:56 +1000

    added checks for uncalled functions

--- test.zig
+++ test.zig
@@ -1006,11 +1006,17 @@ fn refAllDeclsRecursiveExcerptC(comptime T: type) void {
         .@"struct", .@"enum", .@"union", .@"opaque" => refAllDeclsRecursiveExcerptC(@field(T, decl.name)),
         else => {},
       }
-    } else if (@typeInfo(@TypeOf(field)) == .@"fn") { // TODO: this currently causes compiler crash
+    } else if (@typeInfo(@TypeOf(field)) == .@"fn") {
       var should_run: bool = false;
       _ = &should_run;
       if (should_run) {
-        if (ArgsTuple(@TypeOf(field))) |Args| _ = &@call(.auto, field, comptime initType(Args));
+        if (ArgsTuple(@TypeOf(field))) |Args| {
+          _ = &@call(.auto, field, comptime initType(Args));
+        } else comptime {
+          const name = std.fmt.comptimePrint("{s}.{s}", .{@typeName(T), decl.name});
+          // if (skipFunctions.get(name)) @compileError(std.fmt.comptimePrint("Skipping {s}: {s}\n", .{name, @typeName(@TypeOf(field))}));
+          if (!skipFunctions.get(name)) @compileError(std.fmt.comptimePrint("Can't call {s}: {s}\n", .{name, @typeName(@TypeOf(field))}));
+        }
       }
     }
   }
@@ -1020,4 +1026,165 @@ test {
   refAllDeclsRecursiveExcerptC(onnx);
   testing.refAllDeclsRecursive(@This());
 }
+
+/// HashMap implementation used internally while parsing.
+/// This is used for key replacement (${...})
+/// This is a barebones implementation, it uses 8 bits for the fingerprint
+/// unlike the 7 in zig's standard hashmap because we don't require toombstones
+///
+/// I chose to use write this instead of using the standard hashmap because
+/// the standard implementation does not work at comptime, and has toombstones
+/// which are not needed for this use case. We would need to use a context variant
+/// of the hash map to prevent a new allocation for each value and it would result
+/// in same amount of bloat more or less. Besides, this implementation should be
+/// slightly faster (hopefully;) and works at comptime as well. Also, converting
+/// the standard to ComptimeEnvType / EnvType would need rehashing which this
+/// implementation does not need.
+fn HashMap(is_const: bool) type {
+  return struct {
+    const Size = u32;
+    pub const String = []const u8;
+    pub const KV = struct { key: []const u8 };
+    const default_max_load_percentage = 64;
+
+    // This is the start of our allocated block
+    keys: if (is_const) []const ?String else []?String = &.{},
+    // These will be at the end of our allocated block, 0 means unused.
+    meta: if (is_const) []const u8 else []u8 = &.{},
+    /// Length for our keys, values, and meta arrays
+    cap: Size = 0,
+    // How many elements are in use
+    size: Size = 0,
+    // How many elements are available, this is used to reduce the number of instructions needed for the grow check
+    available: Size = 0,
+
+    pub fn initSlice(keys: []const []const u8) HashMap(true) {
+      var self = @This().init(keys.len * default_max_load_percentage / 100 + 1);
+      for (keys) |key| self.put(key);
+      return self.toConst();
+    }
+
+    pub fn init(cap: Size) @This() {
+      @setEvalBranchQuota(1000_000);
+      const c = std.math.ceilPowerOfTwo(Size, cap) catch 16;
+      return .{
+        .keys = blk: { var keys = [_]?String{null} ** c; break :blk &keys; },
+        .meta = blk: { var meta = [_]u8{0} ** c; break :blk &meta; },
+        .cap = c,
+        .available = c * default_max_load_percentage / 100,
+      };
+    }
+
+    fn getHFP(key: []const u8) std.meta.Tuple(&.{u64, u8}) {
+      const h = std.hash_map.StringContext.hash(undefined, key);
+      const fp: u8 = @intCast(h >> 56);
+      return .{h, fp};
+    }
+
+    fn eqlString(string: String, other: []const u8) bool {
+      return std.mem.eql(u8, string.ptr[0..string.len], other);
+    }
+
+    fn getIndex(self: *const @This(), fingerprint: u8, hash: u64, key: []const u8) usize {
+      var i: usize = @intCast(hash & (self.cap - 1));
+      while (self.keys[i] != null) : (i = (i + 1) & (self.cap - 1)) {
+        if (self.meta[i] == fingerprint and eqlString(self.keys[i].?, key)) break;
+      }
+      return i;
+    }
+
+    pub fn get(self: *const @This(), key: []const u8) bool {
+      @setEvalBranchQuota(1000_000);
+      const hash, const fingerprint = getHFP(key);
+      const i = self.getIndex(fingerprint, hash, key);
+      return self.keys[i] != null;
+    }
+
+    pub fn put(self: *@This(), key: []const u8) void {
+      @setEvalBranchQuota(1000_000);
+      self.grow();
+
+      const hash, const fingerprint = getHFP(key);
+      const i = self.getIndex(fingerprint, hash, key);
+      if (self.keys[i] == null) {
+        self.meta[i] = fingerprint;
+        self.keys[i] = key;
+        self.size += 1;
+        self.available -= 1;
+      }
+    }
+
+    fn grow(old: *@This()) void {
+      @setEvalBranchQuota(1000_000);
+      if (old.available > old.size) return;
+      var self = init(if (old.size == 0) 16 else old.size * 2);
+      self.size = old.size;
+
+      for (old.meta, old.keys) |m, k| {
+        if (k == null) continue;
+        const hash, _ = getHFP(k.?);
+        var i: usize = @intCast(hash & (self.cap - 1));
+        while (self.keys[i] != null) : (i = (i + 1) & (self.cap - 1)) {}
+        self.meta[i] = m;
+        self.keys[i] = k;
+      }
+
+      old.* = self;
+    }
+
+    pub fn toConst(self: *const @This()) HashMap(true) {
+      if (is_const) return self.*;
+      const keys: [self.keys.len]?String = self.keys[0..self.keys.len].*;
+      const meta: [self.meta.len]u8 = self.meta[0..self.meta.len].*;
+      return .{
+        .keys = &keys,
+        .meta = &meta,
+        .cap = self.cap,
+        .size = self.size,
+        .available = self.available,
+      };
+    }
+  };
+}
+
+const skipFunctions = HashMap(false).initSlice(&[_][]const u8{
+  "root.Logging.Interface.fromContext",
+  "root.Model.CompilationOptions.WriteInterface.fromContext",
+  "root.ThreadingOptions.ThreadingInterface.fromContext",
+  "root.Op.Custom.init",
+  "root.Allocator.alloc",
+  "root.Allocator.free",
+  "root.Utils.apiCast",
+  "root.Utils.apiCastTo",
+  "root.Utils.cStr",
+  "root.Utils.cStrTo",
+  "root.Utils.pathCast",
+  "root.Utils.pathCastTo",
+  "root.Utils.cCast",
+  "root.Utils.createOptionsKVL",
+  "root.Value.AsUnion",
+  "root.Value.asUnion",
+  "root.Value.asType",
+  "root.Value.Sub.Sequence.toValue",
+  "root.Value.Sub.Map.toValue",
+  "root.Value.Sub.Optional.toValue",
+  "root.Value.Sub.SparseTensor.toValue",
+  "root.Value.Sub.Tensor.toValue",
+  "root.Value.Sub.Opaque.toValue",
+  "root.Value.Sub.Tensor.getData",
+  "root.Value.Sub.Tensor.at", // Uncalled
+  "root.Value.Sub.Tensor.getDataConst", // Uncalled
+  "root.Value.Sub.Opaque.getData", // Uncalled
+  "root.Ep.Interface.init",
+  "root.Ep.DataTransfer.init",
+  "root.Ep.SyncNotificationImpl.init",
+  "root.Ep.Factory.init",
+  "root.Ep.SyncStream.Impl.init",
+  "root.Ep.NodeCompute.Info.init",
+  "root.Session.runAsync",
+  "root.Op.KernelContext.parallelFor", //Uncalled
+  "root.Model.CompilationOptions.LocationInterface.fromContext", // Uncalled
+  "root.Session.Options.C.appendExecutionProvider", // Uncalled
+  "root.Session.Options.C.appendExecutionProviderV2", // Uncalled
+});
 
